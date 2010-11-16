b13820cd2ccca866be3063707203fda77ebae49a
Peter Landry <plandry@provplan.org>
2010-11-16 17:08:00 -0500

    Rewrite of the weave Django application to support OIC's database-backed config.
    Most of the DataHub specific code has been removed, and the migration history
    has been reset, since many of the migrations reference DataHub models.

--- django/weave/migrations/0001_initial.py
+++ django/weave/migrations/0001_initial.py
@@ -1,109 +1,171 @@
-
+# encoding: utf-8
+import datetime
 from south.db import db
+from south.v2 import SchemaMigration
 from django.db import models
-from webportal.weave.models import *
 
-class Migration:
-    
+class Migration(SchemaMigration):
+
     def forwards(self, orm):
         
+        # Adding model 'Category'
+        db.create_table('weave_category', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='children', null=True, to=orm['weave.Category'])),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=300)),
+        ))
+        db.send_create_signal('weave', ['Category'])
+
+        # Adding model 'AttributeColumn'
+        db.create_table('weave_attributecolumn', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('keyType', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('dataType', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('dataTable', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('geometryCollection', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('year', self.gf('django.db.models.fields.CharField')(max_length=256, null=True, blank=True)),
+            ('min', self.gf('django.db.models.fields.CharField')(max_length=256, null=True, blank=True)),
+            ('max', self.gf('django.db.models.fields.CharField')(max_length=256, null=True, blank=True)),
+            ('connection', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('sqlQuery', self.gf('django.db.models.fields.CharField')(max_length=2048)),
+            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], null=True)),
+            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')(null=True)),
+            ('display_name', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('category', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='indicators', null=True, to=orm['weave.Category'])),
+        ))
+        db.send_create_signal('weave', ['AttributeColumn'])
+
+        # Adding unique constraint on 'AttributeColumn', fields ['name', 'keyType', 'year']
+        db.create_unique('weave_attributecolumn', ['name', 'keyType', 'year'])
+
+        # Adding model 'DataFilter'
+        db.create_table('weave_datafilter', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('file', self.gf('django.db.models.fields.files.FileField')(max_length=100, blank=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
+            ('display', self.gf('django.db.models.fields.BooleanField')(default=True, blank=True)),
+            ('key_unit_type', self.gf('django.db.models.fields.CharField')(max_length=256)),
+        ))
+        db.send_create_signal('weave', ['DataFilter'])
+
+        # Adding model 'DataFilterKey'
+        db.create_table('weave_datafilterkey', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('data_filter', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['weave.DataFilter'])),
+            ('key_value', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal('weave', ['DataFilterKey'])
+
         # Adding model 'ClientConfiguration'
         db.create_table('weave_clientconfiguration', (
-            ('yAttribute', orm['weave.ClientConfiguration:yAttribute']),
-            ('histogramTitle', orm['weave.ClientConfiguration:histogramTitle']),
-            ('layout', orm['weave.ClientConfiguration:layout']),
-            ('name', orm['weave.ClientConfiguration:name']),
-            ('colorAttribute', orm['weave.ClientConfiguration:colorAttribute']),
-            ('mapTitle', orm['weave.ClientConfiguration:mapTitle']),
-            ('binAttribute', orm['weave.ClientConfiguration:binAttribute']),
-            ('id', orm['weave.ClientConfiguration:id']),
-            ('xAttribute', orm['weave.ClientConfiguration:xAttribute']),
-            ('heightAttribute', orm['weave.ClientConfiguration:heightAttribute']),
-            ('barChartTitle', orm['weave.ClientConfiguration:barChartTitle']),
-            ('dataTable', orm['weave.ClientConfiguration:dataTable']),
-            ('sortingAttribute', orm['weave.ClientConfiguration:sortingAttribute']),
-            ('slug', orm['weave.ClientConfiguration:slug']),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50, db_index=True)),
+            ('file', self.gf('django.db.models.fields.files.FileField')(max_length=100, null=True, blank=True)),
         ))
         db.send_create_signal('weave', ['ClientConfiguration'])
-        
-        # Adding model 'DataTable'
-        db.create_table('weave_datatable', (
-            ('description', orm['weave.DataTable:description']),
-            ('keys_query', orm['weave.DataTable:keys_query']),
-            ('id', orm['weave.DataTable:id']),
-            ('name', orm['weave.DataTable:name']),
+
+        # Adding model 'GeometryCollection'
+        db.create_table('weave_geometrycollection', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('keyType', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('importNotes', self.gf('django.db.models.fields.CharField')(max_length=256, blank=True)),
+            ('connection', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('schema', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('tablePrefix', self.gf('django.db.models.fields.CharField')(max_length=256)),
         ))
-        db.send_create_signal('weave', ['DataTable'])
-        
-        # Adding model 'Indicator'
-        db.create_table('weave_indicator', (
-            ('key_field_name', orm['weave.Indicator:key_field_name']),
-            ('table_name', orm['weave.Indicator:table_name']),
-            ('id', orm['weave.Indicator:id']),
-            ('name', orm['weave.Indicator:name']),
-        ))
-        db.send_create_signal('weave', ['Indicator'])
-        
-        # Adding model 'Layout'
-        db.create_table('weave_layout', (
-            ('config_template', orm['weave.Layout:config_template']),
-            ('id', orm['weave.Layout:id']),
-            ('name', orm['weave.Layout:name']),
-        ))
-        db.send_create_signal('weave', ['Layout'])
-        
-    
-    
+        db.send_create_signal('weave', ['GeometryCollection'])
+
+
     def backwards(self, orm):
         
+        # Deleting model 'Category'
+        db.delete_table('weave_category')
+
+        # Deleting model 'AttributeColumn'
+        db.delete_table('weave_attributecolumn')
+
+        # Removing unique constraint on 'AttributeColumn', fields ['name', 'keyType', 'year']
+        db.delete_unique('weave_attributecolumn', ['name', 'keyType', 'year'])
+
+        # Deleting model 'DataFilter'
+        db.delete_table('weave_datafilter')
+
+        # Deleting model 'DataFilterKey'
+        db.delete_table('weave_datafilterkey')
+
         # Deleting model 'ClientConfiguration'
         db.delete_table('weave_clientconfiguration')
-        
-        # Deleting model 'DataTable'
-        db.delete_table('weave_datatable')
-        
-        # Deleting model 'Indicator'
-        db.delete_table('weave_indicator')
-        
-        # Deleting model 'Layout'
-        db.delete_table('weave_layout')
-        
-    
-    
+
+        # Deleting model 'GeometryCollection'
+        db.delete_table('weave_geometrycollection')
+
+
     models = {
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'weave.attributecolumn': {
+            'Meta': {'unique_together': "(('name', 'keyType', 'year'),)", 'object_name': 'AttributeColumn'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'indicators'", 'null': 'True', 'to': "orm['weave.Category']"}),
+            'connection': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True'}),
+            'dataTable': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'dataType': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'geometryCollection': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'keyType': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'max': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'min': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True'}),
+            'sqlQuery': ('django.db.models.fields.CharField', [], {'max_length': '2048'}),
+            'year': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'})
+        },
+        'weave.category': {
+            'Meta': {'object_name': 'Category'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '300'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['weave.Category']"})
+        },
         'weave.clientconfiguration': {
-            'barChartTitle': ('django.db.models.fields.CharField', [], {'default': "'Bar Chart'", 'max_length': '100'}),
-            'binAttribute': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'config_as_bin'", 'to': "orm['weave.Indicator']"}),
-            'colorAttribute': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'config_as_color'", 'to': "orm['weave.Indicator']"}),
-            'dataTable': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['weave.DataTable']"}),
-            'heightAttribute': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'config_as_height'", 'to': "orm['weave.Indicator']"}),
-            'histogramTitle': ('django.db.models.fields.CharField', [], {'default': "'Histogram'", 'max_length': '100'}),
+            'Meta': {'object_name': 'ClientConfiguration'},
+            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'layout': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['weave.Layout']"}),
-            'mapTitle': ('django.db.models.fields.CharField', [], {'default': "'Rhode Island'", 'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
-            'sortingAttribute': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'config_as_sorting'", 'to': "orm['weave.Indicator']"}),
-            'xAttribute': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'config_as_x'", 'to': "orm['weave.Indicator']"}),
-            'yAttribute': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'config_as_y'", 'to': "orm['weave.Indicator']"})
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'})
         },
-        'weave.datatable': {
-            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+        'weave.datafilter': {
+            'Meta': {'object_name': 'DataFilter'},
+            'display': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'keys_query': ('django.db.models.fields.TextField', [], {}),
+            'key_unit_type': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
         },
-        'weave.indicator': {
+        'weave.datafilterkey': {
+            'Meta': {'object_name': 'DataFilterKey'},
+            'data_filter': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['weave.DataFilter']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'key_field_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'table_name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+            'key_value': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'weave.layout': {
-            'config_template': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+        'weave.geometrycollection': {
+            'Meta': {'object_name': 'GeometryCollection'},
+            'connection': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+            'importNotes': ('django.db.models.fields.CharField', [], {'max_length': '256', 'blank': 'True'}),
+            'keyType': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'schema': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'tablePrefix': ('django.db.models.fields.CharField', [], {'max_length': '256'})
         }
     }
-    
+
     complete_apps = ['weave']
--- django/weave/models.py
+++ django/weave/models.py
@@ -8,10 +8,7 @@ from django.core.files.storage import FileSystemStorage
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
-
 from weave.fields import PercentField
-from indicators.models import Indicator
-#from dictionary.models import Variable
 
 class Category(models.Model):
     parent = models.ForeignKey('self',null=True,blank=True,related_name='children')
@@ -20,65 +17,55 @@ class Category(models.Model):
     def __unicode__(self):
         return u'%s' % (self.name)
 
-class KeyUnitType(models.Model):
-    name = models.CharField(max_length=100,unique=True)
-
-    def get_data_table(self):
-        return DataTable.objects.get(name=self.name)
-    
-    def __unicode__(self):
-        return u'%s' % (self.name)
-
-class DataTableManager(models.Manager):
-    def get_or_create_for_filter(self, data_filter):
-        return self.get_or_create(
-            name="%s - %s" % (data_filter.key_unit_type, data_filter.name),
-            key_unit_type = data_filter.key_unit_type
-        )
-
-class DataTable(models.Model):
-    name = models.CharField(max_length=100)
-    key_unit_type = models.CharField(max_length=100)
-    
-    objects = DataTableManager()
-    
-    def ordered_attribute_columns(self):
-        return self.attributecolumn_set.order_by('display_name', 'year')
-
 class AttributeColumnManager(models.Manager):
     def years(self):
         return self.exclude(year=None).exclude(year='') \
                    .values_list('year', flat=True).order_by('year').distinct()
- 
-class AttributeColumn(models.Model):
+
+class BaseAttributeColumn(models.Model):
+    """
+    Abstract model to represent the fields necessary for Weave's configuration
+    entries.
+
+    The only difference from Weave's schema is the introduction of
+    `unique_together`.
+    """
     DATA_TYPE_CHOICES = (
         ('number', 'number'),
         ('text', 'text')
     )
-    # TODO: Not sure this relation should be here -- now weave depends on the indicators app
-    # Ideally remove this, and let Indicator find its AttributeColumns, not the other way around.
-    # maybe a GenericForeignKey?
+    
+    name = models.CharField(max_length=256)
+    keyType = models.CharField(max_length=256)
+    dataType = models.CharField(max_length=256,choices=DATA_TYPE_CHOICES)
+    dataTable = models.CharField(max_length=256)
+    geometryCollection = models.CharField(max_length=256)
+    year = models.CharField(max_length=256,null=True,blank=True)
+    min = models.CharField(max_length=256,null=True,blank=True)
+    max = models.CharField(max_length=256,null=True,blank=True)
+    connection = models.CharField(max_length=256)
+    sqlQuery = models.CharField(max_length=2048)
 
+    class Meta:
+        unique_together = (
+            ('name', 'keyType', 'year', ),
+        )
+        abstract = True
+
+class AttributeColumn(BaseAttributeColumn):
+    """
+    Additional fields to add functionality on top of BaseAttributeColumn.
+
+    Adds a GFK to allow this `AttributeColumn` to be linked to a source model.
+    Using a Django model will introduce a surrogate key not strictly necessary
+    for Weave.
+    """
     content_type = models.ForeignKey(ContentType, null=True)
     object_id = models.PositiveIntegerField(null=True)
-    source = generic.GenericForeignKey('content_type', 'object_id')
-    
-    #indicator = models.ForeignKey(Indicator,null=True,blank=True)
-    data_table = models.ForeignKey(DataTable)
-    name = models.CharField(max_length=100)
-    display_name = models.CharField(max_length=255)
+    source = generic.GenericForeignKey('content_type', 'object_id')    
+    display_name = models.CharField(max_length=256)
     category = models.ForeignKey('Category',null=True,blank=True,related_name='indicators')
-    key_unit_type = models.CharField(max_length=100)
-    data_type = models.CharField(max_length=6,choices=DATA_TYPE_CHOICES)
-    
-    data_with_keys_query = models.TextField()
-    
-    min = models.IntegerField(null=True,blank=True)
-    max = models.IntegerField(null=True,blank=True)
 
-    # can hold values other than years
-    year = models.CharField(max_length=50,null=True,blank=True)
-    
     objects = AttributeColumnManager()
 
     @models.permalink
@@ -95,17 +82,12 @@ class AttributeColumn(models.Model):
 
     def __unicode__(self):
         return u"%s" % self.full_display_name
-
-    class Meta:
-        unique_together = (
-            ('name', 'key_unit_type', 'year', ),
-        )    
     
 class DataFilter(models.Model):
     file = models.FileField(upload_to='data_filter_files',blank=True)
     name = models.CharField(max_length=100,unique=True)
     display = models.BooleanField(default=True)
-    key_unit_type = models.ForeignKey(KeyUnitType,null=True)
+    key_unit_type = models.CharField(max_length=256)
     
     def get_xml(self):
         from django.template.loader import render_to_string
@@ -193,9 +175,15 @@ class GeometryCollection(models.Model):
     """A geometry collection that's been imported through the Weave Admin
     console.
     """
-    name = models.CharField(max_length=100,null=False,blank=False)
-    key_unit_type = models.ForeignKey(KeyUnitType,null=True)
+    name = models.CharField(max_length=256)
+    keyType = models.CharField(max_length=256)
+    importNotes = models.CharField(max_length=256,blank=True)
+    connection = models.CharField(max_length=256)
+    schema = models.CharField(max_length=256)
+    tablePrefix = models.CharField(max_length=256)
     
     def __unicode__(self):
         return u'Geometry Collection: %s' % (self.name, )
+
+
 
