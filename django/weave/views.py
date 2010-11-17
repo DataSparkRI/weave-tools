a3cc7113e6091d6b1ac42b28716429c93bcc18fb
Peter Landry <plandry@provplan.org>
2010-11-17 18:27:30 -0500

    fixed typo

--- django/weave/views.py
+++ django/weave/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
-from weave.models import ClientConfiguration, DataFilter \
+from weave.models import ClientConfiguration, DataFilter, \
                             KeyUnitType, DataTable
 from django.core.urlresolvers import reverse
 #from dictionary.models import Variable
