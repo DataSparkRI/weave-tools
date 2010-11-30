024bfcc22f08194ac399e2994b09fb95a1e18fe8
Peter Landry <plandry@provplan.org>
2010-11-30 16:22:57 -0500

    Typo

--- django/weave/admin.py
+++ django/weave/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
 from weave.models import ClientConfiguration, GeometryCollection, \
-                            AttributeColumn)
+                            AttributeColumn
 
 class ClientConfigurationAdmin(admin.ModelAdmin):
     exclude = ('file', )
