0f8da1783af84be87a8901f48f7e43abde17ac8a
Peter Landry <plandry@provplan.org>
2010-11-30 13:43:50 -0500

    Moved views that weren't specific to Weave out of the app.

--- django/weave/urls.py
+++ django/weave/urls.py
@@ -4,9 +4,7 @@ from weave import views
 from django.conf import settings
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name="weave-index"),
     url(r'^client_configurations/(?P<config_slug>[\w-]+).xml$', views.client_config, name="weave-client_config"),
-    url(r'^sqlconfig.xml', views.sqlconfig, name='weave-sqlconfig'),
     url(r'^hierarchy.xml', views.hierarchy, name='weave-hierarchy'),
     url(r'^services.xml', direct_to_template, {
         'template': 'services.xml',
