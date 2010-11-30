31401573bb72265cc55bf95d365dec3ace96453b
Peter Landry <plandry@provplan.org>
2010-11-30 14:04:43 -0500

    Removed test for WeaveExporter, which was moved out of the weave app.

--- django/weave/tests.py
+++ django/weave/tests.py
@@ -31,64 +31,6 @@ class BugFixTests(TestCase):
             200
         )
 
-class TimeTranslationTest(TestCase):
-    def test_school_year(self):
-        """
-        Tests that "School Year 2005-2006" gets turned into "SY05-06" in weave.
-        """
-        from indicators.models import Indicator, IndicatorData
-        from weave.load import run
-        from weave.models import AttributeColumn
-        
-        i = Indicator.objects.create(
-            name='test',
-            display_name='test'
-        )
-        IndicatorData.objects.create(indicator=i, time_type='School Year', time_key = "2005-2006", key_unit_type = "School", key_value = "00001", data_type = 'numeric', numeric = 100)
-        
-        run()
-
-        self.failUnlessEqual(AttributeColumn.objects.all().count(), 1)
-        self.failUnlessEqual(AttributeColumn.objects.all()[0].year, 'SY05-06')
-
-    def test_calendar_year(self):
-        """
-        Tests that "Calendar Year 2005" gets turned into "2005" in weave.
-        """
-        from indicators.models import Indicator, IndicatorData
-        from weave.load import run
-        from weave.models import AttributeColumn
-        
-        i = Indicator.objects.create(
-            name='test',
-            display_name='test'
-        )
-        IndicatorData.objects.create(indicator=i, time_type='Calendar Year', time_key = "2005", key_unit_type = "School", key_value = "00001", data_type = 'numeric', numeric = 100)
-        
-        run()
-
-        self.failUnlessEqual(AttributeColumn.objects.all().count(), 1)
-        self.failUnlessEqual(AttributeColumn.objects.all()[0].year, '2005')
-
-    def test_blank_time_type(self):
-        """
-        Tests that blank time types can pass through type conversion.
-        """
-        from indicators.models import Indicator, IndicatorData
-        from weave.load import run
-        from weave.models import AttributeColumn
-        
-        i = Indicator.objects.create(
-            name='test',
-            display_name='test'
-        )
-        IndicatorData.objects.create(indicator=i, time_type=None, time_key = None, key_unit_type = "School", key_value = "00001", data_type = 'numeric', numeric = 100)
-        
-        run()
-
-        self.failUnlessEqual(AttributeColumn.objects.all().count(), 1)
-        self.failUnlessEqual(AttributeColumn.objects.all()[0].year, '')
-
 class DataFilterTest(TestCase):
     fixtures = ['data_filters.json', ]
     def test_display(self):
